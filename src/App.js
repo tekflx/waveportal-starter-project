dae281b82a5a89b1863bfe43e3a8880dfd919b4e
mancfactor <frankmancina@gmail.com>
2021-11-21 00:40:25 -0500

    Updated site text

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
 
         <div className="bio">
         I am Frank and I worked on some of the biggest entertainment websites in the world, pretty cool right? Connect your Ethereum wallet and wave at me!
+        <p>Just wanted to say thanks to Farza and all buildspace team, you are doing amazing work!!</p>
         </div>
 
         <button className="waveButton" onClick={wave}>
