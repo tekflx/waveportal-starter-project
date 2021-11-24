5c6f54618f12eae1cea4669e94c7d0ad06216a98
mancfactor <frankmancina@gmail.com>
2021-11-24 00:55:42 -0500

    Added new files

---
+++ build/precache-manifest.b0e9d46352080aca9c41f09011e74305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b85ec18975258afbca6256c01cf2b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3347281a14ba9ce336d3",
+    "url": "/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "3347281a14ba9ce336d3",
+    "url": "/static/js/main.d6af2ee8.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
